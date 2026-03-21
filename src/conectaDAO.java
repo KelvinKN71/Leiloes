990e0c5603c10fdc724ff1393be4cf038b9e5df7
KelvinKN71 <kelvim.007@outlook.com>
2026-03-21 18:13:53 -0300

    Configura conexão com bando de dados leiloes

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,11 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(
+    "jdbc:mysql://localhost:3306/leiloes",
+    "root",
+    "Familia02141@"
+);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
